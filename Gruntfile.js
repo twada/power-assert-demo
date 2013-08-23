cacefa24a58450040a8d753c769b5038807516b9
Takuto Wada <takuto.wada@gmail.com>
2013-08-23 15:25:53 +0900

    Run power_assert task by default.

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,4 +113,6 @@ module.exports = function(grunt) {
     grunt.registerTask('mocha_expect', ['mochaTest:mocha_expect']);
     grunt.registerTask('power_assert', ['clean:power_assert', 'espower:power_assert', 'mochaTest:power_assert']);
     grunt.registerTask('power_coffee', ['clean:power_coffee', 'espower_csredux:power_coffee', 'mochaTest:power_coffee']);
+
+    grunt.registerTask('default', ['power_assert']);
 };
